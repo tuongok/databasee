1ce277e39b931551d84e9f62c0cf70929796b9b6
tuongok <danganhtuongg@gmail.com>
2025-03-24 19:31:13 +0700

    server
    oke

--- server.js
+++ server.js
@@ -1,20 +1,12 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('database.json');
+const router = jsonServer.router('database.json', { readonly: true }); // Thêm option này
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
-
 server.use(router);
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
-});
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-// Thêm dòng này để Vercel nhận diện
 module.exports = server;
